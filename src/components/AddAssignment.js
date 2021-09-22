59ef6846bbd395dc55d8cde900598c919a1a65aa
Rowena <rterrado@csumb.edu>
2021-09-21 20:32:06 -0700

    Initial commit

---
+++ src/components/AddAssignment.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Link } from 'react-router-dom'
+import Cookies from 'js-cookie';
+import Button from '@material-ui/core/Button';
+import {DataGrid} from '@material-ui/data-grid';
+import {SERVER_URL} from '../constants.js'
+
+// NOTE:  for OAuth security, http request must have
+//   credentials: 'include' 
+//
+
+class AddAssignment extends Component {
+    constructor(props) {
+      super(props);
+	  this.state = {assignmentName:'', dueDate:'', courseId:''};
+	this.inputChanged = this.inputChanged.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+	};
+	
+	inputChanged = (event) => {
+	   this.setState({[event.target.name]:event.target.value});
+	   }
+ 
+  handleSubmit = (event) => {
+	event.preventDefault();
+	const token = Cookies.get('XSRF-TOKEN');
+    fetch(`${SERVER_URL}/gradebook`, 
+      {  
+        method: 'POST', 
+		headers: {'Accept': 'application/json','Content-Type': 'application/json', 'X-XSRF-TOKEN': token},
+		body: JSON.stringify({assignmentName:this.state.assignmentName, dueDate: this.state.dueDate,courseId: this.state.courseId})
+      } )
+      .then(res => {
+          if (res.ok) {
+            toast.success("Assignment added successfully.", {
+            position: toast.POSITION.BOTTOM_LEFT
+            });
+            this.setState({assignmentName:'', dueDate:'', courseId:''});
+          } else {
+            toast.error("Add assignment failed.", {
+            position: toast.POSITION.BOTTOM_LEFT
+            });
+            console.error('Put http status =' + res.status);
+      }})
+	.catch(err => console.error(err)); 
+	
+      }
+      
+  render() {
+  	return(
+  	<div className="formContainer">
+  	<h1>Add an Assignment</h1>
+     <form onSubmit={this.handleSubmit}>
+		<label>Assignment name:</label><br/>
+			<input type="text" name="assignmentName" value={this.state.assignmentName} onChange={this.inputChanged}/><br/><br/>
+		<label>Assignment due date (YYYY-MM-DD):</label><br/>
+			<input type="text" name="dueDate" value={this.state.dueDate} onChange={this.inputChanged}/><br/><br/>
+		<label>Course ID:</label><br/>
+			<input type="text" name="courseId" value={this.state.courseId} onChange={this.inputChanged}/><br/><br/>
+		
+		<input type="submit" value="Add Assignment" />
+		</form>
+	 <ToastContainer autoClose={1500} />   
+	</div>
+		)
+
+}
+}
+
+export default AddAssignment;
